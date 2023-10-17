ab9130fbc4e24191d480806bf440ab1b30c44b93
AndrewSamaev <andrey@hubstart.io>
2023-10-17 12:59:03 +0300

    initial commit with vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div>Card weather</div>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    //HelloWorld,
+  },
+  data() {
+    return {
+      test: "Test",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+}
+</style>
